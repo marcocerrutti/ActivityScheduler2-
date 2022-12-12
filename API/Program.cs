a12a7f3cb54d4d0191f71a505194768023a23c5c
Nwaigwe Uchenna <hermessuffice@gmail.com>
2022-12-12 08:37:41 +0100

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt =>
+         {
+             opt.AddPolicy("CorsPolicy", policy =>
+             {
+                 policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+             });
+         });
 
 var app = builder.Build();
 
@@ -23,9 +30,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 //app.UseHttpsRedirection();
 
-//app.UseAuthorization();
+app.UseAuthorization();
 
 app.MapControllers();
 
