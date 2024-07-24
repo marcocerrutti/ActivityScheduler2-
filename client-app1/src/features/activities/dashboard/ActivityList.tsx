80ebb09aa951ff6fa794b49ad0362900603917a9
Uchenna Nwaigwe <stupagha@gmail.com>
2024-07-24 08:06:31 +0100

    End of Section 6

--- client-app1/src/features/activities/dashboard/ActivityList.tsx
+++ client-app1/src/features/activities/dashboard/ActivityList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import { Activity } from '../../../app/models/activity';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
 
@@ -7,9 +7,15 @@ interface Props{
     activities: Activity[];
     selectActivity: (id: string)=> void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-function ActivityList({activities, selectActivity, deleteActivity}: Props) {
+function ActivityList({activities, selectActivity, deleteActivity, submitting}: Props) {
+    const [target, setTarget] = useState('');
+    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string){
+        setTarget(e.currentTarget.name);
+        deleteActivity(id);
+    }
     return (
         <Segment>
             <Item.Group divided>
@@ -24,7 +30,14 @@ function ActivityList({activities, selectActivity, deleteActivity}: Props) {
                        </Item.Description>
                        <Item.Extra>
                           <Button onClick={()=>selectActivity(activity.id)} floated='right' content="View" color='blue'/>
-                          <Button onClick={()=>deleteActivity(activity.id)} floated='right' content="Delete" color='red'/>
+                          <Button 
+                               name={activity.id}
+                               loading={submitting && target === activity.id} 
+                               onClick={(e)=> handleActivityDelete(e, activity.id)} 
+                               floated='right'
+                               content="Delete" 
+                               color='red'
+                           />
                           <Label basic content={activity.category} />
                        </Item.Extra>
                     </Item.Content>
